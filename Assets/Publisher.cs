b7fdc465797af6d9d6b08e8ce0f1bdbc3befdcad
Rhys van der Waerden <rhys.vdw@gmail.com>
2020-01-22 14:51:17 +1100

    Event leak test

---
+++ Assets/Publisher.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Publisher : MonoBehaviour
+{
+    public Subscriber SubscriberPrefab;
+    public event Action<string> OnTest;
+
+    public IEnumerator Start()
+    {
+        var subscriber = Instantiate(SubscriberPrefab);
+        subscriber.Inititalize(nameof(subscriber), this);
+        var method = (Action) subscriber.Method;
+
+        yield return null;
+
+        OnTest("Created");
+        Debug.Log($"subscriber = {subscriber}, method = {method}");
+
+        Destroy(subscriber.gameObject);
+
+        yield return null;
+
+        OnTest("Destroyed");
+        Debug.Log($"subscriber = {subscriber}, method = {method}");
+    }
+}
---
+++ Assets/Subscriber.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Subscriber : MonoBehaviour
+{
+    string _name;
+
+    public void Inititalize(string name, Publisher publisher)
+    {
+        _name = name;
+        publisher.OnTest += HandleTest;
+    }
+
+    public void Method() {
+        Debug.Log($"{_name}: {this}");
+    }
+
+    void HandleTest(string text)
+    {
+        Debug.Log($"{_name}: {text}");
+    }
+}
